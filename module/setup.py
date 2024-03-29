ebee9417679aa40ec3cfb09c82395d79cbe4520d
Riley Duggan <riley.duggan214@gmail.com>
2024-03-29 16:32:07 -0700

    Moved class code into its own module

---
+++ module/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wordleanalysis",
+    version="0.0.1",
+    author="Riley Duggan",
+    author_email="riley.duggan214@gmail.com",
+    description="Code for analysing the online game wordle",
+    packages=find_packages()
+)
