7b4dec5278cda806f00f91cd173b33437d924915
Riley Duggan <riley.duggan214@gmail.com>
2026-04-03 22:32:25 +0100

    Started implementing wordle inputs

--- src/nyt_games/web_app/plotly_dash_ui.py
+++ src/nyt_games/web_app/plotly_dash_ui.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, Input, Output
+from dash import dcc,Dash, html, Input, Output, State
 import dash_ag_grid as dag
 import pandas as pd
 
@@ -17,91 +17,73 @@ df = pd.DataFrame({
     "letter_5_score": [0, -1, -1, -1, -1],
 })
 
-grid = dag.AgGrid(
-    id="quickstart-grid",
-    rowData=df.to_dict("records"),
-    columnDefs=[{"field": i} for i in df.columns],
-    defaultColDef={"minWidth":100, "maxWidth": 100},
-    columnSize="sizeToFit",
-)
-
-cell_style = {
-    "styleConditions": [
-        {
-            "condition": "params.data.letter_1_score >= 2",
-            "style": {"backgroundColor": "#79A86B"},
-        },
-        {
-            "condition": "params.data.letter_1_score == 2",
-            "style": {"backgroundColor": "#79A86B"},
-        },
-        {
-            "condition": "params.data.letter_1_score == 1",
-            "style": {"backgroundColor": "#C5B565"},
-        },
-        {
-            "condition": "params.data.letter_1_score == 0",
-            "style": {"backgroundColor": "#797C7E"},
-        },
-        {
-            "condition": "params.data.letter_1_score == -1",
-            "style": {"backgroundColor": "#D3D6DA"},
-        },
-    ],
-    "defaultStyle": {"backgroundColor": "#D3D6DA", "textAlign": "center"},
-}
-
 column_defs = []
 for column in df.columns:
     col_num = int(column[7:8])
-    column_defs.append(
-        {
-            "field": column, 
-            'cellStyle': {
-                "styleConditions": [
-                    {
-                        "condition": f"params.data.letter_{col_num}_score == 2",
-                        "style": {"backgroundColor": "#79A86B"},
-                    },
-                    {
-                        "condition": f"params.data.letter_{col_num}_score == 1",
-                        "style": {"backgroundColor": "#C5B565"},
-                    },
-                    {
-                        "condition": f"params.data.letter_{col_num}_score == 0",
-                        "style": {"backgroundColor": "#797C7E"},
-                    },
-                    {
-                        "condition": f"params.data.letter_{col_num}_score == -1",
-                        "style": {"backgroundColor": "#D3D6DA"},
-                    },
-                ],
-                "defaultStyle": {"backgroundColor": "#D3D6DA", "textAlign": "center"},
-            }
-        }
-    )
+    is_score_column = column[-6:] == '_score'
+    column_def = {
+        "field": column, 
+        'cellStyle': {
+            "styleConditions": [
+                {
+                    "condition": f"params.data.letter_{col_num}_score == 2",
+                    "style": {"backgroundColor": "#79A86B"},
+                },
+                {
+                    "condition": f"params.data.letter_{col_num}_score == 1",
+                    "style": {"backgroundColor": "#C5B565"},
+                },
+                {
+                    "condition": f"params.data.letter_{col_num}_score == 0",
+                    "style": {"backgroundColor": "#797C7E"},
+                },
+                {
+                    "condition": f"params.data.letter_{col_num}_score == -1",
+                    "style": {"backgroundColor": "#D3D6DA"},
+                },
+            ],
+            "defaultStyle": {"backgroundColor": "#D3D6DA", "textAlign": "center"},
+        },
+    }
+    if is_score_column:
+        column_def['hide'] = 'true'
+    
+    column_defs.append(column_def)
 
 default_cell_details = "text-center"
 
 grid = dag.AgGrid(
-    id="quickstart-grid",
+    id="wordle-grid",
     rowData=df.to_dict("records"),
     columnDefs=column_defs,
     defaultColDef={"minWidth":50, "maxWidth": 50},
     columnSize="sizeToFit",
-    dashGridOptions={"animateRows": False}
+    dashGridOptions={"animateRows": False, "headerHeight":0, "editable": True, "flex": 1}
 )
 
-app.layout = html.Div([html.H4("Wordle"), grid, html.Div(id="quickstart-output")])
+dcc.Input(id="dfalse", type="text", placeholder="Debounce False")
 
+input_field = html.Div(
+    [
+        dcc.Input(id="word_input", type="text", placeholder="", debounce=True),
+        html.Hr(),
+        html.Div(id="number-out"),
+    ]
+)
 
 @app.callback(
-    Output("quickstart-output", "children"), Input("quickstart-grid", "cellClicked")
+    Output("number-out", "children"),
+    Input("word_input", "value"),
+    State("wordle-grid", "rowData")
 )
-def display_cell_clicked_on(cell):
-    if cell is None:
-        return "Click on a cell"
-    return f"clicked on cell value:  {cell['value']}, column:   {cell['colId']}, row index:   {cell['rowIndex']}"
+def update(word_input, row_data):
+    # print(row_data.iloc[:, 0])
+    word_from_table = ''
+    for i in range(1, 6):
+        word_from_table += row_data[0][f'letter_{i}']
+    return f'The input word is: {word_input} and {word_from_table}'
+
+app.layout = html.Div([html.H4("Wordle"), grid, html.Div(id="quickstart-output"), input_field])
 
 
 if __name__ == '__main__':
